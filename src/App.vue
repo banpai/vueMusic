d788e654bc80a9a3f9ba48ccd6259da416b1703a
banpai <banpai@yeah.net>
2017-11-08 16:27:33 +0800

    修改添加vue

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,27 @@
 <template>
-  <div id="app">
-    <div class="m-header">地hi福啊好</div>
-    <img src="./common/images/logo.png">
-    <router-view></router-view> 
+  <div id="app" @touchmove.prevent>
+    <m-header></m-header>
+    <tab></tab>
+    <keep-alive>
+      <router-view></router-view>
+    </keep-alive>
+    <player></player>
   </div>
 </template>
 
-<script>
-export default {
-  name: 'app'
-}
+<script type="text/ecmascript-6">
+  import MHeader from 'components/m-header/m-header'
+  import Player from 'components/player/player'
+  import Tab from 'components/tab/tab'
+
+  export default {
+    components: {
+      MHeader,
+      Tab,
+      Player
+    }
+  }
 </script>
 
 <style scoped lang="stylus" rel="stylesheet/stylus">
-@import "~common/stylus/variable"
-.m-header
-    color: $color-background
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
